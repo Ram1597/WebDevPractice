931aed2020141981a8fcbd5b9c792e69fb28b341
Ram1597 <m.sriramachandrachowdari@gmail.com>
2024-04-01 10:35:51 +0530

    Hotstar v1.2

--- Practice/Hotstar Clone/home.js
+++ Practice/Hotstar Clone/home.js
@@ -1,4 +1,4 @@
-setTimeout(sliderFunction,1000);
+setTimeout(sliderFunction,500);
 
 function sliderFunction()
 {
@@ -20,6 +20,58 @@ function sliderFunction()
     i++;
    },2000);
 
+   showMovies();
 
     
+}
+
+function showMovies(){
+    const movies = [
+        {
+          name: "RRR",
+          rating: 8,
+          img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/7642/1307642-v-3d3b6c61f97e",
+        },
+        {
+          name: "Goodluck Jerry	",
+          rating: 7,
+          img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/901/1310901-v-e9763c24f44d",
+        },
+        {
+          name: "MSD",
+          rating: 8.5,
+          img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/old_images/vertical/MOVIE/3314/1770003314/1770003314-v",
+        },
+        {
+          name: "Doctor Strange",
+          rating: 9,
+          img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/9948/1279948-v-cc9471178e40",
+        },
+        {
+          name: "Ford vs Ferrari",
+          rating: 8.7,
+          img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/3150/813150-v",
+        },
+        {
+          name: "Masaan",
+          rating: 8.8,
+          img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/old_images/vertical/MOVIE/7441/1000087441/1000087441-v",
+        },
+        {
+          name: "The lion king",
+          rating: 8.4,
+          img: "https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_3x/sources/r1/cms/prod/5242/875242-v",
+        },
+      ];
+
+
+      movies.forEach( (e)=>{
+        let {img}=e;
+        let image=document.createElement("img");
+        image.src=img;
+        let movie=document.createElement("div");
+        let homepageshows=document.querySelector("#homepageshows");
+        movie.append(image);
+        homepageshows.append(movie);
+      });
 }
